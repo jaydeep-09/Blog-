a92568386b7304c76407cb7c4c9b887a4a5e73a5
JayDeep singh <jaydeepsingh.70881@gmail.com>
2023-10-16 02:52:04 +0530

    new page for a post has created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//jshint esversion:6
+// jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ =require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -49,7 +51,7 @@ app.post("/compose",function(req,res){
 
   const post={
 
-    Title :req.body.composeTitle,
+    Title :_.capitalize(req.body.composeTitle),
     Text :req.body.composeText
 
   };
@@ -61,6 +63,23 @@ app.post("/compose",function(req,res){
 
   res.redirect("/");
   
+})
+
+app.get("/post/:tittle",function(req,res){
+  
+  const newpost=_.capitalize(req.params.tittle);
+  var temp=0;
+  for(var i=0;i<posts.length;i++){
+    if(posts[i].Title===newpost){
+      console.log("match found...");
+
+      res.render('post',{postdata:posts[i]});
+      temp=1;
+    }
+  };
+
+  if(temp!=1)res.redirect("/");
+
 })
 
 
